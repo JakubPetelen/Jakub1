5995fd84fb613896584c56077daf54fa32e58296
Jakub Petelen <petelen@outlook.com>
2024-09-17 10:42:19 +0200

    prvy commit

---
+++ src/app/podmienky/page.tsx
@@ -0,0 +1,11 @@
+
+
+import Typography from '@mui/material/Typography';
+import Container  from "@mui/material/Container";
+
+export default function TermsConditions() {
+  return (
+  <Typography> About Page</Typography>
+  );
+    
+}
