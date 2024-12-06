7c38c50fcfc3a6093496a6488a410ba57139c9ab
JakubPetelen <petelen@outlook.com>
2024-12-06 10:56:02 +0100

    nic zajimave

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -7,7 +7,6 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
-//export const metadata = { title: 'Domovská stránka | KamNaKavu'};
 
 export default function Home() {
   const { data: session} = useSession();
@@ -15,15 +14,14 @@ export default function Home() {
 
   useEffect(() => {
     if (session) {
-      // Redirect to '/prispevky' after session is available
       router.push('/prispevok');
     }
   }, [session, router]);
   if (!session) {
     return (
       <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" minHeight="100vh">
-        <Typography variant="h3" align="center"><strong>Neprihlásený</strong> používateľ</Typography>
-        <Typography variant="h5" align="center">ak chceš pokračovať, prosím <strong>prihlás sa</strong> alebo si <strong>sprav účet</strong> na našej stránke.</Typography>
+        <Typography variant="h5">Neprihlásený užívateľ</Typography>
+        <Typography variant="h5">prosím prihlás sa alebo si sprav účet.</Typography>
       </Box>        
       );
   }
